febf4f918400c5daca4250d92e72dd69b16c12db
yaseenomer <yaseeno373@gmail.com>
2020-10-23 02:58:49 +0200

    fix some issue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,8 +24,6 @@ window.axios = require('axios');
 
 window.axios.defaults.withCredentials = true;
 
-window.axios.defaults.baseURL = 'https://technical-test-yaseen-omer.herokuapp.com/'
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
