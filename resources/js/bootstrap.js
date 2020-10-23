2a7540855813f7cb2290484c9963e0d4b739844e
yaseenomer <yaseeno373@gmail.com>
2020-10-23 02:37:03 +0200

    'ffff'

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,6 +24,8 @@ window.axios = require('axios');
 
 window.axios.defaults.withCredentials = true;
 
+window.axios.defaults.baseURL = 'https://technical-test-yaseen-omer.herokuapp.com/'
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
